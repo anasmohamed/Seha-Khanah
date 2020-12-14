7fa4544aa8a883fb9718dc44e2339301c7977f46
Anas Mohamed <anaskamal659@gmail.com>
2020-12-14 10:35:00 +0200

    Initial Commit

---
+++ Seha Khanah/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seha Khanah
+//
+//  Created by Anas on 12/14/20.
+//  Copyright © 2020 Anas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
