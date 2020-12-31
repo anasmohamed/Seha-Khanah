b944865755db2725c0fe4a88a724d2288fa7a43d
Anas Mohamed <anaskamal659@gmail.com>
2020-12-31 16:41:14 +0200

    lab details view

---
+++ Seha Khanah/Presentaion/DetailsScreensScenen/LabsDetailsScene/View/Cells/BookingDatesCollectionViewCell/BookingDatesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookingDatesCollectionViewCell.swift
+//  Seha Khanah
+//
+//  Created by Anas on 12/31/20.
+//  Copyright © 2020 Anas. All rights reserved.
+//
+
+import UIKit
+
+class BookingDatesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
