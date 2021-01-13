0b97f8e00ea1c7da1dd984e659014cd2c46ccd55
Anas Mohamed <anaskamal659@gmail.com>
2021-01-13 15:15:44 +0200

    user bookings model

--- Seha Khanah/Models/MyAppiontments.swift
+++ Seha Khanah/Models/MyAppiontments.swift
@@ -9,12 +9,35 @@
 import Foundation
 import SwiftyJSON
 class MyAppiontments: Codable {
-    var photo: String?
-     var nameEn: String?
-     init?(withJSON data: JSON) {
-         
-         self.photo = data["featured"].stringValue
-         self.nameEn = data["name_en"].stringValue
-         
-     }
+    var id: String?
+    var clientId :String?
+    var doctorId:String?
+    var bookingDate : String?
+    var statusId:String?
+    var doctor : DoctorDetails?
+    var status : BookingStatus?
+    init?(withJSON data: JSON) {
+        
+        self.id = data["id"].stringValue
+        self.clientId = data[""].stringValue
+        self.doctorId = data["id"].stringValue
+        self.bookingDate = data[""].stringValue
+        self.statusId = data["name_en"].stringValue
+        self.id = data["id"].stringValue
+        self.doctor = DoctorDetails(withJSON: data["doctor"])
+        self.status = BookingStatus(withJSON: data["status"])
+        
+    }
+}
+class BookingStatus: Codable {
+    var id: String?
+    var nameAr:String?
+    var nameEn:String?
+    init?(withJSON data: JSON) {
+        
+        self.id = data["id"].stringValue
+        self.nameAr = data["name_ar"].string
+        self.nameEn = data["name_en"].stringValue
+        
+    }
 }
